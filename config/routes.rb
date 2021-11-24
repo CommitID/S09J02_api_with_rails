e15d5e594a6930bb413494c6f6a7176916246d03
‘CommitID’ <hanxcity@gmail.com>
2021-11-24 12:08:45 +0100

    add path to: app# and constraints: (request)

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   root to: "articles#index"
   resources :articles
   resources :users
+
+    get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
